d484dfb238d3cd15d8cea0af57ccb9b86c6217f9
Bjørn Thomas <159652469+BjornMThomas@users.noreply.github.com>
2024-05-03 18:14:32 -0700

    Add files via upload

---
+++ bjorn212.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+int main() {
+// std::cout << "Enter Money Here: ";
+// int Money;
+// std::cin >> Money;
+// double pi = 3.14;
+// double MoneyPi = Money * 3.14;
+// std::cout << "Money Times Pi: " << MoneyPi << std::endl;
+
+std::cout << "A wild goblin approaches what do you do?" << std::endl
+          << "(1. attack, 2. run, 3. hide 4. stats)" << std::endl
+          << "Choose wisely: ";
+int attack = 1;
+int run = 2;
+int hide = 3;
+int stats = 4;
+int choice;
+
+double health = 200;
+double defense = 0;
+double hunger = 100;
+
+std::cin >> choice;
+if (choice == attack) {
+  std::cout << "You chose to fight the wild goblin" << std::endl
+            << "You lunge at the goblin and hit it in the chest" << std::endl
+            << "The goblin loses 20hp" << std::endl;
+  choice = 0;
+} else if (choice == run) {
+  std::cout << "You try to escape but as you run the goblin hits you with a rock from behind" << std::endl
+            << "You escape successfully, yet you lose 25hp" << std::endl;
+  health = health - 20;
+  choice = 0;
+} else if (choice == hide) {
+  std::cout << "You hide behind a bush until the goblin walks away and you escape successfully" << std::endl;
+} else if (choice == stats) {
+  std::cout << "Health: " << health << std::endl
+            << "Defense: " << defense << std::endl
+            << "Hunger: " << hunger << std::endl;
+} else {
+  std::cout << "You sit and do nothing" << std::endl;
+}
+return 0;
+}
